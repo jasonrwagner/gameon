7b20059726d2bd7fd7645c391127e9e808956d90
jasonrwagner <wagner.jason.r@gmail.com>
2013-03-29 10:08:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameon::Application.config.secret_token = '84208f03ab82ceb6ee6a277917035b632b07f6e5f61ac1422157748838b44135e4c59402151d2bef1fb7c25f17f9b50a3febdedfc61500d389ed5e629aa7fd69'
