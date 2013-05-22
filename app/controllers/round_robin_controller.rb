24f7ccef1b1730dd94cfcac7cb6f1af9c0fc6b15
jasonrwagner <wagner.jason.r@gmail.com>
2013-05-22 14:41:12 -0400

    Removed index.html

--- app/controllers/round_robin_controller.rb
+++
@@ -1,9 +0,0 @@
-class RoundRobinController < ApplicationController
-
-  def populate
-  	 @tournament = Tournament.find(params[:tournament_id])
-  	 @round_robin_teams = @tournament.teams.all
-  	 @current_round_robin_teams = @round_robin_teams.shuffle
-
-  end
-end
